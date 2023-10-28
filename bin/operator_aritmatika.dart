a77035cf85ccfb98d54604f01d22bc586cec831d
4rthemis <syahrul.uix@gmail.com>
2023-10-29 06:56:14 +0700

    operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,22 @@
+/**
+ * Operator Aritmatika
+ * + pertambahan
+ * -, -expression pengurangan atau negatif
+ * * perkalian
+ * / pembagian, hasil double
+ * ~/ pembagian, hasil int
+ *  % sisa bagi
+ */
+
+void main(){
+  
+
+  var pertambahan = 1 + 1;
+  print(pertambahan);
+
+  var perkalian = 20 * 20;
+  print(perkalian);
+
+  var pengurangan = -2 - 10;
+  print(pengurangan);
+}
