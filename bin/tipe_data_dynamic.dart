02c52a67e71aac03f5d86e3b9d994974a546ca0c
4rthemis <syahrul.uix@gmail.com>
2023-10-28 21:57:02 +0700

    tipe data dynamic

---
+++ bin/tipe_data_dynamic.dart
@@ -0,0 +1,17 @@
+/**
+ * Dynamic
+ * kadang ada kebutuhan kita ingin membuat variabke yang bisa menampung semua jenis tipe data
+ * Pada kasus ini, kita bisa menggunakan tipe data dynamic
+ */
+
+
+void main(){
+  dynamic name = 'abdul';
+  print(name);
+
+  name = 10;
+  print(name);
+
+  name = 10.1;
+  print(name);
+}
