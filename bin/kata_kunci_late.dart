f505d8049f05117fa5547da6a3dec499b949a461
4rthemis <syahrul.uix@gmail.com>
2023-10-28 20:59:44 +0700

    kata kunci late

---
+++ bin/kata_kunci_late.dart
@@ -0,0 +1,10 @@
+void main() {
+  late var value = getValue();
+  print('variable sudah dibuat');
+  print(value);
+}
+
+String getValue() {
+  print("tampilkan fungsi getValue");
+  return 'anjay';
+}
