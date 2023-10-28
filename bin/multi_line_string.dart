bba1bc096c193af7e0a86fac843353f46b916488
4rthemis <syahrul.uix@gmail.com>
2023-10-28 21:26:29 +0700

    Multiline String

---
+++ bin/multi_line_string.dart
@@ -0,0 +1,18 @@
+//Multiline String
+
+// ignore_for_file: unused_local_variable
+
+// ignore: slash_for_doc_comments
+/**
+ * Kadang kita butuh membuat string yang sangat panjang, sehingga jika kita buat dalam satu baris kode,
+ * kode tersebut akan terlalu panjang.
+ * String mendukung pembuatan data secara multiline,
+ * caranya dengan menggunakan petik satu atau petik dua sebanyak tiga karakter
+ */
+
+void main() {
+  var texts = '''
+String yang bisa sangat panjang
+untuk membuat string seperti ini gunakan kutip 3 kali
+''';
+}
