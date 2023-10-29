a2cafe4875c310aba27e29cc112f15998d3d6f9d
4rthemis <syahrul.uix@gmail.com>
2023-10-29 22:47:40 +0700

    deklarasi list langsung

---
+++ bin/deklarasi_list_langsung.dart
@@ -0,0 +1,20 @@
+/**
+ * Deklarasi List langsung
+ * list juga menudukung deklarasi data secara langsung ketika pembuatan variable List
+ * ini mempermudah kita ketika membuat list jika memang datanya sudah siap
+ * caranya bisa gunakan :
+ * 
+ */
+
+void main() {
+  // var names = [
+  //   'asep',
+  //   'jamal',
+  //   'bagus',
+  // ];
+
+  // print(names);
+
+  var names = <String> ['asep', 'ridho'];
+  print(names);
+}
