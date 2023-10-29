f545453ebaea7a57376bd70070eefb9cf83892cd
4rthemis <syahrul.uix@gmail.com>
2023-10-29 22:25:36 +0700

    list atau array

---
+++ bin/list.dart
@@ -0,0 +1,13 @@
+/**
+ *  List
+ * List merupakan tipe data yang berisikan kumpulan data
+ *  Di bahasa pemrograman lain, tipe data ini dikinela dengan tipe data Array
+ *  Saat kita memmbuat List, kita perlu mennentukan isi dari tipe data List
+ * Untuk membuat data List, kita bisa menggunakan []
+ * Di Dart, semua tipe data adalah object dimana List sendiri memiliki property, method dan operator
+ */
+
+void main (){
+  List<String> name = ['Abdul', 'Asep', 'Chuaks'];
+  print(name[2]);
+}
