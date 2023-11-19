238c3a8f7e9a8983b88b25017def2f381b01476a
4rthemis <syahrul.uix@gmail.com>
2023-11-19 15:39:17 +0700

    konversi nullable

---
+++ bin/konversi_nullable_to_nonullable.dart
@@ -0,0 +1,22 @@
+void main(){
+  /**
+   * Untuk melakukan konversi tipe data non nullable ke nullable kita bisa langsung masukan data nya saja
+   * Namun untuk melakukan konversi tipe data nullable, kita wajib melakukan null check terlebih dahulu
+   */
+
+
+  // pengecekan bahwa name tidak null
+  // konversi dari yang tidak null ke null
+  String name = 'abdul';
+  String? nullableName = name;
+
+  print(name);
+
+  int? nullableNumber = null;
+
+  // perlu dilakukan null check
+  if(nullableNumber != null){
+    int price = nullableNumber;
+    print(price);
+  }
+}
