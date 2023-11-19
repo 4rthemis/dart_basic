5f627ca1084efc8ec10cd1e9caa63d38b26eac26
4rthemis <syahrul.uix@gmail.com>
2023-11-19 13:50:17 +0700

    tipe data null

---
+++ bin/tipe_data_null.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  // tipe data null
+  /**
+   * Kata kunci null merupakan tipe data representasi data kosong
+   * secara default, saat kita membuat variable, data harus diisi, maka variable tidak bisa digunakan
+   * namun jika kita membuat sebuah variable bisa diisi data null, kita bisa tambahkan karakter tanda tanya(?) seperti :
+   * String? name= null;
+   */
+
+
+  String? name;
+
+  print(name);
+}
