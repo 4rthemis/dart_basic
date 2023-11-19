aaae792fe46ea74b805d42fab1316c4796a7704a
4rthemis <syahrul.uix@gmail.com>
2023-11-19 22:19:48 +0700

    while

---
+++ bin/while.dart
@@ -0,0 +1,13 @@
+void main() {
+  /** While Loop
+   *  while loop adalah versi perulangan yang lebih sederhana dibanding for loop
+   *  Di while loop hanya terdapat kondisi perulangan tanpa ada init statement dan post statement
+   */
+
+  var age = 12;
+
+  if (age >= 10) {
+    print('perulangan ke -$age');
+    age++;
+  }
+}
