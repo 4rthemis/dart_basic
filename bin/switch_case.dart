bd484046e3c959b25d46d203cf251cc214e52dfb
4rthemis <syahrul.uix@gmail.com>
2023-11-19 13:45:24 +0700

    switch case

---
+++ bin/switch_case.dart
@@ -0,0 +1,21 @@
+void main(){
+
+
+var nilai='B';
+
+switch(nilai){
+  case 'A' :
+  print("nilai anda sempurna");
+  break;
+  case 'B' :
+  print('Nilai anda bagus');
+  break;
+  case 'C':
+  print('nilai anda cukup');
+  break;
+  default:
+  print('anda tidak lulus');
+
+}
+
+}
