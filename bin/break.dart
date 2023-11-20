edb2b09a664014e8c3fd8a0b5bbc39cf9fe20492
4rthemis <syahrul.uix@gmail.com>
2023-11-21 06:47:37 +0700

    break

---
+++ bin/break.dart
@@ -0,0 +1,16 @@
+void main(){
+    var counter = 1;
+
+
+    while(true){
+      print('perulangan ke-$counter');
+      counter++;
+
+    // jika variable counter melebihi 20, maka hentikan
+      if(counter >= 20){
+        break;
+      }
+
+
+    }
+}
