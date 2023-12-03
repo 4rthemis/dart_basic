cd33927088545b8df01e9fecc9d32741173bdbe7
4rthemis <syahrul.uix@gmail.com>
2023-12-03 22:25:40 +0700

    higher order function

---
+++ bin/higher_order_function.dart
@@ -0,0 +1,22 @@
+/// Higher Order Function
+///
+/// Higher Order Function adalah function yang menggunakan function sebagai variable, parameter atau return value
+/// Penggunaan Higher ORder Function kadang berguna ketika kita ingin membuat function yang geneeral
+/// dan ingin mendapatkan input yang flexible berupa function, yang bisa dideklarasikan oleh pengguna ketika memanggil function tersebut
+
+void main() {
+  sayHi('eko', filteredBadWord);
+}
+
+void sayHi(String name, String Function(String) filter) {
+  var filteredName = filter(name);
+  print('hi $filteredName');
+}
+
+String filteredBadWord(String name) {
+  if (name == "gila") {
+    return '******';
+  } else {
+    return name;
+  }
+}
