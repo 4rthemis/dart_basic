82f472bfec74a3e1f5901296e518fdd7d9d722b0
4rthemis <syahrul.uix@gmail.com>
2023-12-03 23:04:40 +0700

    scrope

---
+++ bin/scope.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = 'jamal';
+
+
+  sayHi(String age){
+    var age ='13';
+
+  }
+  //  variable age tidak bisa diakses karena berada diluar function sayHi
+  // print('$age');
+
+  print('hi $name');
+}
